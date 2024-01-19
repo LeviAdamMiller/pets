6dcee1aae3702c1160c5bedc473ed2ad99302fa3
Levi Miller <Miller.Levi@student.greenriver.edu>
2024-01-18 20:39:09 -0500

    added styles

--- tmp/3gc754glv42sc.3k0ijvsyid8g8.php
+++ tmp/3gc754glv42sc.3k0ijvsyid8g8.php
@@ -3,14 +3,17 @@
 <head>
   <meta charset="UTF-8">
   <title>Pets!</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
   <link rel="stylesheet" href="styles/pets-style.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
 </head>
-<body>
+<body id="background">
 <div class="container">
   <h1 id="heading">Winston!</h1>
-  <img id='winston' src="images/dog.jpeg" alt="winstonPhoto">
+  <img id='winston' src="images/FuzzyWinston.jpg" alt="winstonPhoto">
 
+</div>
+<div class="container">
+<a id="charityLink" href="https://www.seattlehumane.org/"> Click Here to donate to Seattle Humane Society!</a>
 </div>
 </body>
 </html>

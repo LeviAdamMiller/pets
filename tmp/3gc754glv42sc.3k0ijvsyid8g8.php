ddab5f16a72fc6e88e6ab554fce85068bd973432
Levi Miller <Miller.Levi@student.greenriver.edu>
2024-01-18 20:03:42 -0500

    commiting

---
+++ tmp/3gc754glv42sc.3k0ijvsyid8g8.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Pets!</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+  <link rel="stylesheet" href="styles/pets-style.css">
+</head>
+<body>
+<div class="container">
+  <h1 id="heading">Winston!</h1>
+  <img id='winston' src="images/dog.jpeg" alt="winstonPhoto">
+
+</div>
+</body>
+</html>
